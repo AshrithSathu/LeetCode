f0e66e79949f10511fd35ed1d84d913b2d7c9c8e
Ashrith Sathu <119956578+AshrithSathu@users.noreply.github.com>
2024-07-09 17:17:22 +0530

    Time: 1 ms (40.28%), Space: 2.1 MB (76.39%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.rs
@@ -0,0 +1,32 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn swap_pairs(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        if head.is_none() || head.as_ref().unwrap().next.is_none() {
+            return head;
+        }
+
+        let mut head = head;
+
+        let mut second = head.as_mut().unwrap().next.take();
+
+        head.as_mut().unwrap().next = Solution::swap_pairs(second.as_mut().unwrap().next.take());
+        second.as_mut().unwrap().next = head;
+
+        return second
+    }
+}
