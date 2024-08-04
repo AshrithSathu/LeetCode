75fd760cc51e39bdeed83444478b85d8eaec4804
punto <119956578+AshrithSathu@users.noreply.github.com>
2024-08-04 17:23:14 +0530

    Time: 4 ms (29.94%), Space: 62.7 MB (40.22%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int n = nums1.length;
+        int m = nums2.length;
+        int i = 0 , j = 0;
+        while( n>0 && m>0 ){
+            if(nums1[i]>nums2[j]){
+                j++;
+                m--;
+            }else if(nums1[i]<nums2[j]){
+                i++;
+                n--;
+            }else if(nums1[i]==nums2[j]){
+                return nums1[i];
+            }
+        }
+        return -1;
+        
+    }
+}
