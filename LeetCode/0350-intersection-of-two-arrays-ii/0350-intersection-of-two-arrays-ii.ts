5bc282dd56fd71890ead426dcc75da7e9c223e60
punto <119956578+AshrithSathu@users.noreply.github.com>
2025-07-04 03:57:28 +0530

    Time: 83 ms (5.36%), Space: 65.7 MB (5.36%) - LeetArchive

---
+++ LeetCode/0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.ts
@@ -0,0 +1,19 @@
+function intersect(nums1: number[], nums2: number[]): number[] {
+    const map = new Map()
+    if(nums1.length < nums2.length){
+        return intersect(nums2,nums1)
+    }
+    for(let i = 0 ; i < nums2.length ; i++){
+        map.set(nums2[i],(map.has(nums2[i]) ? map.get(nums2[i]) + 1 : 1))
+    }
+    let answer = new Array()
+
+    for(let i = 0 ; i < nums1.length ; i++){
+        if(map.get(nums1[i])>0){
+            answer.push(nums1[i])
+            console.log(answer)
+            map.set(nums1[i],map.get(nums1[i]) - 1)
+        }
+    }
+    return answer
+};
