ccfadf1b9b8cb6d108f7e026984443c1028121da
punto <119956578+AshrithSathu@users.noreply.github.com>
2026-01-10 21:03:39 +0530

    Time: 27 ms (48.06%), Space: 87 MB (6.77%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
@@ -0,0 +1,11 @@
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+    const map = new Map()
+    for ( let i = 0 ; i < nums.length ; i++){
+        if ( map.has(nums[i]) && (Math.abs(map.get(nums[i]) - i) <= k) ){
+            return true;
+        }else{
+            map.set(nums[i],i)
+        }
+    }
+    return false
+};
