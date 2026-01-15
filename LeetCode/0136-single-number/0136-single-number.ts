7fdeac402d97fdfa31ba5381552218e21cf2426a
punto <119956578+AshrithSathu@users.noreply.github.com>
2026-01-15 15:19:26 +0530

    Delete LeetCode directory

--- LeetCode/0136-single-number/0136-single-number.ts
+++
@@ -1,8 +0,0 @@
-function singleNumber(nums: number[]): number {
-    let answer = nums[0];
-
-    for(let i = 1 ; i < nums.length ; i++ ){
-        answer = answer ^ nums[i];
-    }
-    return answer
-};
